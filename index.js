fd0af5937d7c3f5461476104821c264d8827f8a3
Hugh <hugorochalves@gmail.com>
2022-04-29 18:25:26 -0300

    Interface do netflix, programa da dio carrefour web developer

---
+++ index.js
@@ -0,0 +1,13 @@
+function abrirModal() {
+    document.getElementById('modal').style.top = "0";
+} 
+function fecharModal() {
+    document.getElementById('modal').style.top = "-100%"
+}
+
+function abririnfoModal() {
+    document.getElementById('modal-info').style.top = "0";
+} 
+function fecharinfoModal() {
+    document.getElementById('modal-info').style.top = "-100%"
+}
